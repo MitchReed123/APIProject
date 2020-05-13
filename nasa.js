896b8bb91ed8c232ffbd2130accacdc6f2f6ffd1
Mitchell Reed <mitchellreed@Mitchells-MacBook-Pro.local>
2020-05-13 14:01:17 -0400

    Final push for 5/13/200

--- nasa.js
+++ nasa.js
@@ -2,7 +2,8 @@ const baseUrl = "https://eonet.sci.gsfc.nasa.gov/api/v3/events?";
 const key = "W3ic4GJkvIT9VcudyjfjIJ2hjquaAqDyOdTKwYxy";
 let url;
 
-const searchDate = document.querySelector(".date");
+const startDate = document.querySelector(".start-date");
+const endDate = document.querySelector(".end-date");
 const searchForm = document.querySelector("form");
 const submitBtn = document.querySelector(".submit");
 const nextBtn = document.querySelector(".next");
@@ -10,27 +11,34 @@ const prevBtn = document.querySelector(".prev");
 const nav = document.querySelector("nav");
 const section = document.querySelector("section");
 
-nav.style.display = "none";
+// nav.style.display = "none";
 
 let pageNumber = 0;
 let displanav = false;
 console.log("PageNumber", pageNumber);
 
 searchForm.addEventListener("submit", fetchResults);
-nextBtn.addEventListener("click", nextPage);
-prevBtn.addEventListener("click", previousPage);
+// nextBtn.addEventListener("click", nextPage);
+// prevBtn.addEventListener("click", previousPage);
+startDate.addEventListener("input", startDateInfo);
+endDate.addEventListener("input", endDateInfo);
 
 function fetchResults(e) {
-  console.log(e);
+  //   console.log(e);
+  console.log(pageNumber);
   e.preventDefault();
-  url = `${baseUrl}?api-key=${key}&page=${pageNumber}&q=${searchDate.value}limit=15`;
+  url = `${baseUrl}?api-key=${key}&page=${pageNumber}`;
   console.log("URL:", url);
 
-  if (searchDate.value !== " ") {
-    console.log(searchDate.value);
-    url += "&date=" + searchDate.value;
+  if (startDate.value !== "") {
+    console.log(startDate.value);
+    url += "&start=" + startDate.value;
   }
 
+  if (endDate.value !== "") {
+    console.log(endDate.value);
+    url += "&end=" + endDate.value;
+  }
   fetch(url)
     .then(function (result) {
       console.log(result);
@@ -43,68 +51,75 @@ function fetchResults(e) {
 }
 
 function displayResults(json) {
-  while (section.firstChild) [section.removeChild(section.firstChild)];
+  while (section.firstChild) {
+    section.removeChild(section.firstChild);
+  }
 
   let nasa = json.events;
+
   console.log(nasa);
 
   if (nasa.length === 0) {
     console.log("No Results");
   } else {
     for (let n = 0; n < nasa.length; n++) {
-      let id = document.createElement("h1");
+      let geoNasa = nasa[n].geometry;
+
+      let id = document.createElement("article");
       let title = document.createElement("h2");
       let link = document.createElement("a");
-      let geometry = document.createElement("div");
-      let date = document.createElement("p");
+      let sources = document.createElement("div");
       let clearfix = document.createElement("div");
 
       let current = nasa[n];
       console.log("Current:", current);
 
       link.href = current.link;
-      console.log(link);
+      console.log((link.target = "_blank"));
       link.textContent = current.link;
 
-      id.textContent = "Title: ";
+      title.innerText = current.title;
 
-      for (let i = 0; i < current.title.length; i++) {
-        let span = document.createElement("span");
-        span.textContent += current.title[i] + "";
-        id.appendChild(span);
+      for (const dataSet of geoNasa) {
+        console.log(dataSet);
+        let magUnit = document.createElement("p");
+        let magValue = document.createElement("p");
+        magUnit.innerText = dataSet.magnitudeUnit;
+        magValue.innerText = dataSet.magnitudeValue;
+
+        sources.appendChild(magUnit);
+        sources.appendChild(magValue);
       }
-
       clearfix.setAttribute("class", "clearfix");
-
       id.appendChild(title);
-      title.appendChild(link);
-      id.appendChild(geometry);
-      id.appendChild(date);
+      id.appendChild(link);
+      //   id.appendChild(mag);
+      id.appendChild(sources);
       id.appendChild(clearfix);
       section.appendChild(id);
     }
   }
 
-  if (nasa.length === 10) {
-    nav.style.display = "block";
-  } else {
-    nav.style.display = "none";
-  }
+  //   if (nasa.length === 10) {
+  //     nav.style.display = "block";
+  //   } else {
+  //     nav.style.display = "none";
+  //   }
 }
 
-function nextPage(e) {
-  pageNumber++;
-  fetchResults(e);
-  console.log("Page Number:", pageNumber);
-}
+// function nextPage(e) {
+//   pageNumber++;
+//   fetchResults(e);
+//   console.log("Page Number:", pageNumber);
+// }
 
-function previousPage(e) {
-  if (pageNumber > 0) {
-    pageNumber--;
-    fetchResults(e);
-  } else {
-    return;
-  }
-  fetchResults(e);
-  console.log("Page:", pageNumber);
-}
+// function previousPage(e) {
+//   if (pageNumber > 0) {
+//     pageNumber--;
+//     fetchResults(e);
+//   } else {
+//     return;
+//   }
+//   fetchResults(e);
+//   console.log("Page:", pageNumber);
+// }
